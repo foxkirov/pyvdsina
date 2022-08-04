5367e7e10a1c346a4327f59fd74e47510738e258
antonyp <foxkirov@mail.ru>
2022-08-04 14:54:43 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='pyvdsina',
+    version='0.1.0',
+    description='VDSina API wrapper Package',
+    url='http://example.com',
+    install_requires=['requests==2.28.1'],
+    packages=['pyvdsina'],
+)
